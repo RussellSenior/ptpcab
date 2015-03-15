dfc20b76b7bab8c2f5fd2813b21f170956ed1f0f
Russell Senior <russell@personaltelco.net>
2015-03-15 00:45:21 -0700

    support recent kernels merged overlayfs
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- ptp-utils/files/since-last-flash.sh
+++ ptp-utils/files/since-last-flash.sh
@@ -1,1 +1,1 @@
-uci show | diff -u /etc/uci-firstboot - ; for f in $(find /overlay -type f | sed 's|/overlay/||' | grep -v etc/config | grep -v etc/uci-firstboot) ; do diff -uN /rom/$f /$f ; done
+uci show | diff -u /etc/uci-firstboot - ; for f in $(find /overlay/upper -type f | sed 's|/overlay/upper/||' | grep -v etc/config | grep -v etc/uci-firstboot) ; do diff -uN /rom/$f /$f ; done
