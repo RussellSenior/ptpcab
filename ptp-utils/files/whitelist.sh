c8302475a67a264df39fa4240dcbfb435ce55321
Russell Senior <russell@personaltelco.net>
2021-02-04 01:00:21 -0800

    ptp-utils: add persistence to the whitelist.sh output
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- ptp-utils/files/whitelist.sh
+++ ptp-utils/files/whitelist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for i in $@ ; do
-	grep -i "$i" /tmp/dhcp.leases | awk '{ print "uci add dhcp host ; uci set dhcp.@host[-1].mac=" $2 " ; uci set dhcp.@host[-1].ip=" $3 " ; uci set dhcp.@host[-1].name=" $4 " ; uci commit dhcp ; /usr/nocatauth/bin/access.fw permit " $2, $3, "Member" }'
+	grep -i "$i" /tmp/dhcp.leases | awk '{ print "uci add dhcp host ; uci set dhcp.@host[-1].mac=" $2 " ; uci set dhcp.@host[-1].ip=" $3 " ; uci set dhcp.@host[-1].name=" $4 " ; uci commit dhcp ; /usr/nocatauth/bin/access.fw permit " $2, $3, "Member ; echo /usr/nocatauth/bin/access.fw permit " $2, $3, "Member >> /usr/nocatauth/bin/initialize.fw" }'
 done
