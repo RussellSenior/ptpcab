f0dfaa1d9c6dac4ed985e2cc98bd10f44198d60f
Russell Senior <russell@personaltelco.net>
2024-12-16 04:01:00 -0800

    ptp-utils: add openwrt version to wiki-info.sh
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- ptp-utils/files/wiki-info.sh
+++ ptp-utils/files/wiki-info.sh
@@ -6,6 +6,7 @@ if [ -f /tmp/sysinfo/board_name ]
 then 
   echo '  * Device:' $(cat /tmp/sysinfo/board_name)
 fi 
+echo '  * OpenWrt:' $(cat /etc/openwrt_version)
 echo '  * IPv4:' $(ip -4 -o addr show dev $(uci get network.pub.device) | awk '{ print $4 }')
 for j in $(brctl show br-pub | awk '{ print $NF }' | grep wlan) ; do 
   echo $j $(iwinfo $j info | awk '$2 ~ /ESSID:/ { print $3 } $0 ~ /Access Point/ { print $3 } $0 ~ /Mode: Master/ { print $4 }')
