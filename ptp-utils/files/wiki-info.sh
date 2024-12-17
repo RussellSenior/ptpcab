bd61659d7c727d7b6ef3f804d4a95c724602d16b
Russell Senior <russell@personaltelco.net>
2024-12-17 01:30:31 -0800

    ptp-utils: more tweaks to wiki-info.sh
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- ptp-utils/files/wiki-info.sh
+++ ptp-utils/files/wiki-info.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo ' *' $(cat /proc/sys/kernel/hostname) 
+echo ' * Hostname:' '`'$(cat /proc/sys/kernel/hostname)'`' 
 if [ -f /tmp/sysinfo/board_name ] 
 then 
   echo '  * Device:' $(cat /tmp/sysinfo/board_name)
@@ -10,5 +10,5 @@ echo '  * OpenWrt:' $(cat /etc/openwrt_version)
 echo '  * IPv4:' $(ip -4 -o addr show dev $(uci get network.pub.device) | awk '{ print $4 }')
 for j in $(brctl show br-pub | awk '{ print $NF }' | grep wlan) ; do 
   echo $j $(iwinfo $j info | awk '$2 ~ /ESSID:/ { print $3 } $0 ~ /Access Point/ { print $3 } $0 ~ /Mode: Master/ { print $4 }')
-done | awk '{ print "  *",$1 ; print "   * SSID:",$2 ; print "   * BSSID:", $3 ; print "   * Channel:",$4 }'
+done | awk '{ print "  * Interface:",$1 ; print "   * SSID:",$2 ; print "   * BSSID:", $3 ; print "   * Channel:",$4 }'
 
