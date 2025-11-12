35e122e86de5a610b34c35784f75d50c75e58d57
Russell Senior <russell@personaltelco.net>
2025-11-11 22:01:49 -0800

    ptp-utils: even more improved wiki-info.sh
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- ptp-utils/files/wiki-info.sh
+++ ptp-utils/files/wiki-info.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pub_device=$(uci get network.pub.device)
 
 echo ' * Hostname:' '`'$(cat /proc/sys/kernel/hostname)'`' 
 if [ -f /tmp/sysinfo/board_name ] 
@@ -9,7 +10,7 @@ else
   echo '  * Device:' $(awk '$1 ~ /vendor_id/ && $3 ~ /AuthenticAMD/ { print "pcengines,alix2" } $1 ~ /vendor_id/ && $0 ~ /Geode by NSC/ { print "soekris,net4826" }' /proc/cpuinfo)
 fi 
 echo '  * OpenWrt:' $(cat /etc/openwrt_version)
-echo '  * IPv4:' $(ip -4 -o addr show dev br-pub | awk '{ print $4 }')
-for j in $(brctl show br-pub | awk '{ print $NF }' | grep wlan) ; do 
+echo '  * IPv4:' $(ip -4 -o addr show dev $pub_device | awk '{ print $4 }')
+for j in $(brctl show $pub_device | awk '{ print $NF }' | grep wlan) ; do 
   echo $j $(iwinfo $j info | awk '$2 ~ /ESSID:/ { print $3 } $0 ~ /Access Point/ { print $3 } $0 ~ /Mode: Master/ { print $4 }')
 done | awk '{ print "  * Interface:",$1 ; print "   * SSID:",$2 ; print "   * BSSID:", $3 ; print "   * Channel:",$4 }'
